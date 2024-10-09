49f9cc5e00db0d772dfd254c54ec28c703fff6de
MikolajGierszewskii <155561030+MikolajGierszewskii@users.noreply.github.com>
2024-10-09 16:17:01 +0200

    Add files via upload

---
+++ gierszewski_zewnetrzny_zadanie_2.js
@@ -0,0 +1,3 @@
+document.write("Podaję czas".fontcolor("blue").fontsize(6)+"</br>");
+document.write("Aktualny czas: ".fontcolor("red").strike().fontsize(6)+"</br>");
+document.write(document.lastModified);
